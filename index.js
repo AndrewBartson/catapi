30cd5ea405721029752080a80e18bc7fd0366532
CADBOT <charliec364@gmail.com>
2018-01-12 15:30:01 -0800

    update to MVC

--- index.js
+++ index.js
@@ -2,43 +2,31 @@ let express = require('express')
 let bodyParser = require('body-parser')
 let app = express()
 
-let model = require('./model')
-console.log(model)
-// equivalent to let model = require('./model/index')
+let controller = require('./controller')
 
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-// Read routes
-app.get('/cats', (req, res) => {
-  let cats = model.getAllCats()
-  res.json({data: cats})
-})
+app.route('/cats')
+  .get(controller.getCat)
+  .post(controller.createCat)
 
-app.get('/cats/:id', (req, res) => {
-  let id = Number(req.params.id)
-  let cat = model.getCat(id)
-  res.json({data: cat})
-})
+app.route('/cats/:id')
+  .get(controller.getCat)
+  .patch(controller.updateCat)
+  .delete(controller.deleteCat)
 
-// Create route
-app.post('/cats', (req, res) => {
-  let newCat = model.createCat(req.body)
-  res.json({data: newCat})
-})
-
-// update route
-app.patch('/cats/:id', (req, res) => {
-  debugger
-  let updatedCat = model.updateCat(req.params.id, req.body)
-  res.json({data: updatedCat})
-})
-
-// destroy route
-app.delete('/cats/:id', (req, res) => {
-  let deletedCat = model.deleteCat(req.params.id)
-  res.json({data: deletedCat})
+/*
+app.get('/cats', controller.getAllCats)
+app.get('/cats/:id', controller.getCat)
+app.post('/cats', controller.createCat)
+app.patch('/cats/:id', controller.updateCat)
+app.delete('/cats/:id', controller.deleteCat)
+*/
+  
+app.use((err, req, res, next) => {
+  res.json({error: err})
 })
 
 app.listen(PORT, () => {
